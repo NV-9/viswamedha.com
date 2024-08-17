cbc34c7d9546e7337c66b18c52fb2e903e3d41e1
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-08-17 20:29:37 +0100

    Added description for user serializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    User serializer for viswamedha.com
+    """
     class Meta:
         model = User
         fields = ['id', 'user_uuid', 'username', 'email_address', 'first_name', 'last_name', 'date_of_birth', 'is_staff', 'is_active', 'is_verified', 'created_at', 'updated_at', 'verification_token', 'reset_password_token', 'reset_instance_token', 'password_reset_at']
