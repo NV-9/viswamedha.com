911d8f531c0ffc6a0dfc353d2547700a15c2a1fe
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-08-17 20:28:59 +0100

    Added blog viewsets to api routes

--- config/api_router.py
+++ config/api_router.py
@@ -2,11 +2,14 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from apps.blog.viewsets import PostViewSet, TagViewSet
 from apps.users.viewsets import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("posts", PostViewSet)
+router.register("tags", TagViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
