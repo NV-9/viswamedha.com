a54ef23cf7a8081349bc9f2a23d621eda63d7167
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-08-17 22:09:03 +0100

    Added main app to installed apps

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("api/", include("config.api_router")),
-    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('', include('apps.main.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
