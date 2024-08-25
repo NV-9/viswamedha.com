7ccee857e531198710feffd66d9dbb3d37bea7c1
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-08-25 11:05:59 +0100

    Added basic homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home'));
+
 
 function App() {
-
-  return (
-    <>
-      <div>
-      
-      </div>
-      
-    </>
-  )
+	return (
+		<Suspense fallback={<div className="container">Loading...</div>}>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</Suspense>       
+	)
 }
 
-export default App
+export default App;
