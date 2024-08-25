18e70eea66e47633559602af20d590424a4e3fea
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-08-25 12:52:17 +0100

    Styled home page with antd components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import { Typography } from 'antd';
+import { Flex, Typography } from 'antd';
 import { ReactTyped } from "react-typed";
-
+import '../assets/css/home.css';
 const { Title } = Typography;
 
 export default function Home() {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
+        <Flex vertical='true' justify='center' align='center' style={{ height: "100vh"}}>
             <Title style={{ fontSize: '64px', margin: 0 }}>Viswamedha Nalabotu</Title>
             <Title level={2} style={{ margin: 0 }}>I'm a <ReactTyped strings={["Student", "Developer", "Blogger", "Freelancer", "Photographer", "Gamer"]} typeSpeed={100} backSpeed={50} loop /></Title>
-        </div>
+        </Flex>
     );
 }
