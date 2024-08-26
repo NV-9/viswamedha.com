2572e72db09205bb452e4e25d232e6f78833b62f
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-08-26 11:35:07 +0100

    Added lookup fields for blog viewsets

--- apps/blog/viewsets.py
+++ apps/blog/viewsets.py
@@ -7,8 +7,10 @@ from .serializers import PostSerializer, TagSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    lookup_field = 'post_uuid'
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    lookup_field = 'tag_uuid'
