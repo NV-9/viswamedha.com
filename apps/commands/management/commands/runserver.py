65b659d931937ace1c4f4199da020a108066303d
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-09-08 12:33:28 +0100

    Improved runserver command

--- apps/commands/management/commands/runserver.py
+++ apps/commands/management/commands/runserver.py
@@ -7,6 +7,22 @@ from django.core.management.commands.runserver import Command as BaseRunserverCo
 from django.conf import settings
 
 class Command(BaseRunserverCommand):
-    def inner_run(self, *args, **options):
+    help = "Run the server with extra options"
+
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            '--build',
+            action='store_true',
+            dest='build',
+            default=False,
+            help='Run the build command before starting the server'
+        )
+
+    def handle(self, *args, **options):
+        if options['build']:
+            self.run_build()
+        super(Command, self).handle(*args, **options)
+
+    def run_build(self):
         subprocess.run(["npm.cmd", "run", "build"])
-        super(Command, self).inner_run(*args, **options)
