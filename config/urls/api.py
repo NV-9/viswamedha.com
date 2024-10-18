5c707f084afad08f8325aae568045562da20a317
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-10-18 17:36:59 +0100

    Addded api paths for main models

--- config/urls/api.py
+++ config/urls/api.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
 from apps.blog.viewsets import PostViewSet, TagViewSet
+from apps.main.viewsets import PhotoViewSet, ReferenceViewSet
 from apps.users.viewsets import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
@@ -10,6 +11,8 @@ router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 router.register("user", UserViewSet)
 router.register("post", PostViewSet)
 router.register("tag", TagViewSet)
+router.register("photo", PhotoViewSet)
+router.register("reference", ReferenceViewSet)
 
 app_name = "api"
 urlpatterns = router.urls 
