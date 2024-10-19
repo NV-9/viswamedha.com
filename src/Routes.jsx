b21e69120bc5934fc7a5b2f14b01fee27afedc44
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-10-19 21:04:39 +0100

    Added profile route and component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('./pages/Home'));
 const Login = lazy(() => import('./pages/Login'));
+const Profile = lazy(() => import('./pages/Profile'));
 
 export const routes = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login/' element={<Login />} />
+            <Route path='/profile/' element={<Profile />} />
         </Routes>
     )
 }
