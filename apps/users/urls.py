0190387a697e4ab6446120f0d22115d5166d5a5f
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-10-20 11:18:07 +0100

    Added signup and session views

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
 
-from apps.users.views import login_view, logout_view
+from apps.users.views import login_view, logout_view, signup_view, session_view
 
 urlpatterns = [
     path('api/login/', login_view, name='api-login'),
     path('api/logout/', logout_view, name='api-logout'),
+    path('api/signup/', signup_view, name='api-signup'),
+    path('api/session/', session_view, name='api-session'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,10 @@
 from django.views.decorators.http import require_POST
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 import json
+
+from apps.users.models import User
 
 @require_POST
 def login_view(request):
@@ -27,3 +30,36 @@ def logout_view(request):
         return JsonResponse({'detail': 'You\'re not logged in.'})
     logout(request)
     return JsonResponse({'detail': 'Successfully logged out.'})
+
+
+@require_POST
+def signup_view(request):
+    try:
+        data = json.loads(request.body)
+    except:
+        return JsonResponse({'detail': 'Invalid JSON provided.', 'success': False}, status = 401)
+    
+    if User.objects.filter(username = data.get('username')).exists():
+        return JsonResponse({'detail': 'Username already exists.', 'success': False})
+    if User.objects.filter(email_address = data.get('email_address')).exists():
+        return JsonResponse({'detail': 'Email address already exists.', 'success': False})
+    if len(data.get('first_name')) == 0 or len(data.get('last_name')) == 0:
+        return JsonResponse({'detail': 'First and last name(s) must be provided.', 'success': False})
+    if data.get('password') != data.get('confirm_password'):
+        return JsonResponse({'detail': 'Passwords do not match.', 'success': False})
+
+    user = User.objects.create(username = data.get('username'),
+                        email_address = data.get('email_address'),
+                        first_name = data.get('first_name'),
+                        last_name = data.get('last_name'),
+                        date_of_birth = data.get('date_of_birth'))
+    if not user:
+        return JsonResponse({'detail': 'User not created.', 'success': False})
+    user.set_password(data.get('password'))
+    user.save()
+    return JsonResponse({'detail': 'User account created successfully.', 'success': True})
+
+
+@ensure_csrf_cookie
+def session_view(request):
+    return JsonResponse({'isAuthenticated': request.user.is_authenticated})
