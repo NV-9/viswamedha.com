f89ce596705d8187785dddcb719293def4b36b18
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-10-26 20:07:17 +0100

    Added drawer for the navigation menu

---
+++ src/utils/DrawerMenu.jsx
@@ -0,0 +1,44 @@
+import { Drawer } from '@mui/material';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+
+export default function DrawerMenu({ drawerOpen, setDrawerOpen }) {
+    const toggleDrawer = (open) => (event) => {
+		if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+			return;
+		}
+		setDrawerOpen(open);
+	}
+            
+    return (
+        <Drawer open={drawerOpen} onClose={toggleDrawer(false)}>		
+			<Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+				<List>
+					{['Home', 'About'].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+						<ListItemIcon>
+							<HomeIcon />
+						</ListItemIcon>
+						<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+					))}
+				</List>
+				<Divider />
+				<List>
+					{['Login', 'Register'].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+						<ListItemIcon>
+							<HomeIcon />
+						</ListItemIcon>
+						<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+					))}
+				</List>
+			</Box>
+		</Drawer>
+	);
+}
