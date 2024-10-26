9834509a2ab8180c84a738cae77446e33e8eda05
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-10-26 19:42:52 +0100

    Moved routing to utils

---
+++ src/utils/Routing.jsx
@@ -0,0 +1,18 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Home = lazy(() => import('../pages/Home'));
+const Login = lazy(() => import('../pages/Login'));
+const Signup = lazy(() => import('../pages/Signup'));
+const Profile = lazy(() => import('../pages/Profile'));
+
+export default function Routing({ setDrawerOpen }) {
+    return (
+        <Routes>
+            <Route path='/' element={<Home setDrawerOpen={setDrawerOpen}/>} />
+            <Route path='/login/' element={<Login setDrawerOpen={setDrawerOpen}/>} />
+            <Route path='/signup/' element={<Signup setDrawerOpen={setDrawerOpen}/>} />
+            <Route path='/profile/' element={<Profile setDrawerOpen={setDrawerOpen}/>} />
+        </Routes>
+    )
+}
