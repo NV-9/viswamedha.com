d25aba90ec6a72f493ff89abb8b3d17937ccab77
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-11-03 15:30:23 +0000

    Added contact message endpoint

--- config/urls/api.py
+++ config/urls/api.py
@@ -3,12 +3,13 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
 from apps.blog.viewsets import PostViewSet, TagViewSet
-from apps.main.viewsets import PhotoViewSet, ReferenceViewSet
+from apps.main.viewsets import ContactMessageViewSet, PhotoViewSet, ReferenceViewSet
 from apps.users.viewsets import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("user", UserViewSet)
+router.register("contact-message", ContactMessageViewSet)
 router.register("post", PostViewSet)
 router.register("tag", TagViewSet)
 router.register("photo", PhotoViewSet)
