9cd85d5a16df2dbc3d0d26280ec29f71aa45a8fb
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-11-03 17:13:13 +0000

    Added subdomain routing

--- src/utils/DrawerMenu.jsx
+++ src/utils/DrawerMenu.jsx
@@ -3,30 +3,43 @@ import { useNavigate } from 'react-router-dom';
 import { mapping } from './Mapping';
 
 export default function DrawerMenu({ drawerOpen, setDrawerOpen }) {
-	const navigate = useNavigate();
-    const toggleDrawer = (open) => () => {
-        setDrawerOpen(open); 	
-    };
+    const navigate = useNavigate();
+
+    const sortedItems = Object.keys(mapping)
+		.filter(key => mapping[key].order >= 0)
+        .sort((a, b) => mapping[a].order - mapping[b].order) 
+        .reduce((acc, key) => {
+            const itemSubDomain = mapping[key].subdomain || 'default';
+            if (!acc[itemSubDomain]) {
+                acc[itemSubDomain] = [];
+            }
+            acc[itemSubDomain].push(key);
+            return acc;
+        }, {});
 
     return (
-        <Drawer open={Boolean(drawerOpen)} onClose={toggleDrawer(false)}>
-            <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
-                <List>
-					{Object.keys(mapping).map((key) => {
-						const Icon = mapping[key].icon;
-						return (
-							<ListItem disablePadding key={key}>
-								<ListItemButton onClick={() => navigate(mapping[key].path)}>
-									<ListItemIcon>
-										<Icon />
-									</ListItemIcon>
-									<ListItemText primary={key} />
-								</ListItemButton>
-							</ListItem>
-						)
-					})}
-				</List>
-                <Divider />
+        <Drawer open={Boolean(drawerOpen)} onClose={() => setDrawerOpen(false)}>
+            <Box sx={{ width: 250 }} role="presentation" onClick={() => setDrawerOpen(false)} onKeyDown={() => setDrawerOpen(false)}>
+                {Object.keys(sortedItems).map((domain, index) => (
+                    <Box key={domain}>
+                        {index > 0 && <Divider />}
+                        <List>
+                            {sortedItems[domain].map((key) => {
+                                const Icon = mapping[key].icon;
+                                return (
+                                    <ListItem disablePadding key={key}>
+                                        <ListItemButton onClick={() => navigate(mapping[key].path)}>
+                                            <ListItemIcon>
+                                                <Icon />
+                                            </ListItemIcon>
+                                            <ListItemText primary={key} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                );
+                            })}
+                        </List>
+                    </Box>
+                ))}
             </Box>
         </Drawer>
     );
--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,12 +1,19 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { getRootDomain, getSubDomain } from './Domain';
 import { mapping } from './Mapping';
 
 export default function Routing({ setDrawerOpen }) {
+    const [subDomain, setSubDomain] = useState(null);
+
+    useEffect(() => {
+        setSubDomain(getSubDomain());
+    }, []);
+
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                {Object.keys(mapping).map((key) => {
+                {Object.keys(mapping).filter((key) => mapping[key].subdomain == subDomain).map((key) => {
                     const Component = lazy(() => Promise.resolve({ default: mapping[key].component }));
                     return (
                         <Route key={key} path={mapping[key].path} element={<Component setDrawerOpen={setDrawerOpen} />}/>
