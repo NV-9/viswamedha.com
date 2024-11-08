9917a36d8fcac7cad0fbd884a06d5b62bbecfe27
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-11-08 15:32:29 +0000

    Added models to admin, removed groups

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-from apps.main.models import Photo
+from apps.main.models import ContactMessage, Photo, Reference
 
+admin.site.unregister(Group)
+
+admin.site.register(ContactMessage)
 admin.site.register(Photo)
+admin.site.register(Reference)
