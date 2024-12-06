1825724c45001f2101fa2057b1baa4b2ce67a260
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-06 18:31:17 +0000

    Added tutor models to admin

--- apps/tutor/admin.py
+++ apps/tutor/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from apps.tutor.models import Review
+from apps.tutor.models import Course, Event, Lesson, LessonPlan, Review, Student, Subject, Level
 
 admin.site.register(Review)
+admin.site.register(Subject)
+admin.site.register(Level)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(LessonPlan)
+admin.site.register(Event)
