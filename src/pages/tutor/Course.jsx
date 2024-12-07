5060a3ed0f164222b06a9ebc682bc4b739b02797
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-07 20:22:54 +0000

    Added courses and course pages

---
+++ src/pages/tutor/Course.jsx
@@ -0,0 +1,75 @@
+import { Typography, Box, Grid2 as Grid, IconButton, Card, CardContent, CardActions, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
+import { useParams } from 'react-router-dom';
+
+import { API_ENDPOINTS } from '../../utils/Mapping';
+import { ApiRouter } from '../../utils/Api';
+
+export default function Course({ setDrawerOpen }) {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [course, setCourse] = useState(null);
+
+    useEffect(() => {
+        if (id !== undefined && id !== null) {
+            ApiRouter.get(API_ENDPOINTS.COURSE(id))
+            .then(course => setCourse(course))
+            .catch((err) => console.error('Error fetching course:', err));
+        }
+    }, [id]);
+
+    return (
+        <Box sx={{ backgroundColor: 'rgba(5,10,14,1.00)', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', p: 3, px: 10 }}>
+            <IconButton sx={{ position: 'absolute', top: 64, left: 64, color: 'white' }} onClick={setDrawerOpen}>
+                <MenuIcon />
+            </IconButton>
+
+           
+
+            {course && (
+                <>
+                    <Typography variant="h2" sx={{ textAlign: 'center', mt: 2 }} gutterBottom>
+                        {course.name}
+                    </Typography>
+                    <Grid container spacing={4}>
+                        <Grid xs={12} sm={6} md={3} key={course.name} sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Card sx={{ backgroundColor: 'rgba(20, 25, 30, 1.00)', color: 'white'}}>
+                                <CardContent>
+                                    <Typography variant="h6" gutterBottom noWrap sx={{ textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                        {course.name}
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        {course.description}
+                                    </Typography>
+                                    <Typography variant="body2" sx={{ mt: 2 }}>
+                                        Note: Lessons are typically 1 hour long and are tailored to the student's needs, preferably stated in advance. All lessons 
+                                        are conducted online, the exact platform will be discussed with the student. Special requests can be made for in-person lessons, 
+                                        however, this is subject to availability and location.
+                                    </Typography>
+                                    <Typography variant="body2" sx={{ mt: 2 }}>
+                                        The pricing referenced here is the typical cost for this course. However, this can vary depending on various factors
+                                        such as the students's current level, their learning needs, and in rare instances, the complexity of the course. 
+                                        For all lessons, payment isn't required until after a lesson, however, if lessons are cancelled frequently without sufficient notice, 
+                                        pre payment may be required. Payment can be made via bank transfer or PayPal. If you have any questions or concerns, please feel free to
+                                        contact me.
+                                    </Typography>
+                                </CardContent>
+                                <CardActions sx={{ justifyContent: 'space-between', p: 2 }}>
+                                    <Chip label={`£${course.cost}`} sx={{ mb: 1, color: 'white', borderColor: 'white' }} color='white' variant='outlined'/>
+                                    <Button size="small" color="primary" onClick={() => navigate(`/courses/`)}>Back</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+        
+                    </Grid>
+                </>
+            )}
+
+            
+        </Box>
+    );
+}
--- src/pages/tutor/Courses.jsx
+++ src/pages/tutor/Courses.jsx
@@ -1,106 +1,107 @@
-import { Typography, Box, Grid2 as Grid, IconButton } from '@mui/material';
+import { 
+    Typography, Box, Grid2 as Grid, IconButton, Card, CardContent, CardActions, 
+    FormControl, InputLabel, Select, MenuItem 
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import Chip from '@mui/material/Chip';
 
 import { API_ENDPOINTS } from '../../utils/Mapping';
 import { ApiRouter } from '../../utils/Api';
 
-
 export default function Courses({ setDrawerOpen }) {
     const navigate = useNavigate();
     const [courses, setCourses] = useState([]);
     const [subjects, setSubjects] = useState([]);
-    const [selectedSubject, setSelectedSubject] = useState(null);
-    const [selectedCourses, setSelectedCourses] = useState([]);
     const [levels, setLevels] = useState([]);
- 
-    const handleSubjectClick = (name) => {
-        setSelectedSubject(name === selectedSubject ? null : name);
-    }
+    const [selectedSubject, setSelectedSubject] = useState('');
+    const [selectedLevel, setSelectedLevel] = useState('');
+
+    const [filteredCourses, setFilteredCourses] = useState([]);
 
     useEffect(() => {
         ApiRouter.get(API_ENDPOINTS.COURSES())
             .then(setCourses)
             .catch((err) => console.error('Error fetching courses:', err));
-        
+
         ApiRouter.get(API_ENDPOINTS.SUBJECTS())
             .then(setSubjects)
             .catch((err) => console.error('Error fetching subjects:', err));
-        
+
         ApiRouter.get(API_ENDPOINTS.LEVELS())
             .then(setLevels)
             .catch((err) => console.error('Error fetching levels:', err));
     }, []);
 
     useEffect(() => {
-        if (subjects.length > 0) {
-            setSelectedSubject(subjects[0].name);
-            setSelectedCourses(courses.filter(course => course.subject === selectedSubject));
-        }
-    }, [subjects]);
+        let filtered = courses;
 
-    useEffect(() => {
         if (selectedSubject) {
-            setSelectedCourses(courses.filter(course => course.subject === selectedSubject));
+            filtered = filtered.filter(course => course.subject === selectedSubject);
         }
-    }, [selectedSubject]);
 
-    const handleClick = () => {
-        console.info('You clicked the Chip.');
-    };
+        if (selectedLevel) {
+            filtered = filtered.filter(course => course.level === selectedLevel);
+        }
+
+        setFilteredCourses(filtered);
+    }, [courses, selectedSubject, selectedLevel]);
 
     return (
         <Box sx={{ backgroundColor: 'rgba(5,10,14,1.00)', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', p: 3, px: 10 }}>
             <IconButton sx={{ position: 'absolute', top: 64, left: 64, color: 'white' }} onClick={setDrawerOpen}>
                 <MenuIcon />
             </IconButton>
-            
+
             <Typography variant="h2" sx={{ textAlign: 'center', mt: 2 }} gutterBottom>
                 View Courses
             </Typography>
 
-            <Grid container justifyContent="center" sx={{ mt: 4 }}>
-                <Grid item="true" xs={12} md={8}>
-                    <ButtonGroup variant="contained" aria-label="Basic button group" className={"button-group"}>
-                        {subjects.length > 0 && subjects.map((subject) => ( 
-                            <Button key={subject.name} className="parallelogram" variant={subject.name === selectedSubject ? "contained" : "outlined"} 
-                            sx={{ cursor: 'pointer', position: 'relative', transform: 'skew(-20deg)', color: (subject.name === selectedSubject ? '#fdf800' : '#37EBF3'), border: 'none', padding: '15px 30px', fontSize: '16px',zIndex: 1 }} color="#fdf800"
-                            onClick={() => handleSubjectClick(subject.name)}>
-                                <Typography sx={{transform: 'skew(20deg)', width: '200px'}}>{subject.name}</Typography>
-                            </Button>
+            <Box sx={{ display: 'flex', gap: 3, mt: 3, mb: 4, width: '100%', maxWidth: '800px', justifyContent: 'center', flexDirection: { xs: 'column', sm: 'row' } }}>
+                <FormControl fullWidth sx={{ color: 'white', '& .MuiInputLabel-root': { color: 'white' }, '& .MuiOutlinedInput-root': { color: 'white', '& fieldset': { borderColor: 'white' }, '&:hover fieldset': { borderColor: 'white' }, '&.Mui-focused fieldset': { borderColor: 'white' }} }}>
+                    <InputLabel>Subject</InputLabel>
+                    <Select value={selectedSubject} onChange={(e) => setSelectedSubject(e.target.value)} label="Subject">
+                        <MenuItem value="">All Subjects</MenuItem>
+                        {subjects.map((subject, index) => (
+                            <MenuItem key={index} value={subject.name}>
+                                {subject.name}
+                            </MenuItem>
                         ))}
-                    </ButtonGroup>
-                </Grid>
-            </Grid> 
+                    </Select>
+                </FormControl>
 
-            <Grid container direction="column" alignItems="center" justifyContent="center" spacing={4} sx={{ mt: 4 }}>
-                { selectedCourses && selectedCourses.map((course, index) => (
-                    <Grid key={index} item="true">
-                        <Grid container alignItems="center" justifyContent="center" spacing={2}>
-                            <Grid item="true">
-                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                    <Box sx={{ width: 300, height: 80, borderRadius: '90px', backgroundColor: 'rgba(255,255,255,0.1)', display: 'flex', alignItems: 'center', justifyContent: 'center', boxShadow: 3 }}>
-                                        <Typography variant="h6">{course.level}</Typography>
-                                    </Box>
-                                    <Box sx={{ width: 250, height: 80, backgroundColor: '#fdf800', position: 'relative', display: 'flex', alignItems: 'center' }}>
-                                        <Chip label={`£${course.cost}`} variant="outlined" onClick={handleClick} />
-                                        <Typography sx={{ fontSize: '14px', color: '#000' }}>
-                                            {course.description}
-                                        </Typography>
-                                    </Box>
-                                </Box>
-                            </Grid>
-                        </Grid>
-                        {index < selectedCourses.length - 1 && (
-                            <Grid container justifyContent="center" alignItems="center" sx={{ mt: 2 }}>
-                                <ArrowDownwardIcon sx={{ color: 'white', fontSize: 32 }} />
-                            </Grid>
-                        )}
+                <FormControl fullWidth sx={{ color: 'white', '& .MuiInputLabel-root': { color: 'white' }, '& .MuiOutlinedInput-root': { color: 'white', '& fieldset': { borderColor: 'white' }, '&:hover fieldset': { borderColor: 'white' }, '&.Mui-focused fieldset': { borderColor: 'white' } } }}>
+                    <InputLabel>Level</InputLabel>
+                    <Select value={selectedLevel} onChange={(e) => setSelectedLevel(e.target.value)} label="Level">
+                        <MenuItem value="">All Levels</MenuItem>
+                        {levels.map((level, index) => (
+                            <MenuItem key={index} value={level.name}>
+                                {level.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </Box>
+
+            <Grid container spacing={4}>
+                {filteredCourses.map((course, index) => (
+                    <Grid xs={12} sm={6} md={3} key={index} sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Card sx={{ backgroundColor: 'rgba(20, 25, 30, 1.00)', color: 'white', width: 300}}>
+                            <CardContent>
+                                <Typography variant="h6" gutterBottom noWrap sx={{ textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                    {course.name}
+                                </Typography>
+                                <Typography variant="body2">
+                                    {`${course.description.substring(0, 30)}...`}
+                                </Typography>
+                            </CardContent>
+                            <CardActions sx={{ justifyContent: 'space-between', p: 2 }}>
+                                <Chip label={`£${course.cost}`} sx={{ mb: 1, color: 'white', borderColor: 'white' }} color='white' variant='outlined'/>
+                                <Button size="small" color="primary" onClick={() => navigate(`/course/${course.id}`)}>View More</Button>
+                            </CardActions>
+                        </Card>
                     </Grid>
                 ))}
             </Grid>
