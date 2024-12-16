6bb34238b1f4a46cc13ae9dfa1aa7cfdfeb6a3f4
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-16 17:46:47 +0000

    Shortened bool expressions

--- apps/main/permissions.py
+++ apps/main/permissions.py
@@ -3,14 +3,8 @@ from rest_framework import permissions
 class ContactMessagePermission(permissions.BasePermission):
     message = 'Cannot create contact message without authentication.'
 
-    def has_permission(self, request, view):
-        if request.user.is_authenticated and request.user.is_staff:
-            return True
-        if request.method == 'POST':
-            return True
-        return False
+    def has_permission(self, request, view):   
+        return (request.user.is_authenticated and request.user.is_staff) or request.method == 'POST'
     
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and request.user.is_staff:
-            return True
-        return False
+        return request.user.is_authenticated and request.user.is_staff
