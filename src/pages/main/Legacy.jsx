74c23341f37bf232cea4857c4a50a2dfe39dac24
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-18 14:12:18 +0000

    Tweaked legacy pages layout

--- src/pages/main/Legacy.jsx
+++ src/pages/main/Legacy.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from "react-router-dom";
+import { Box, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default function Legacy({ setDrawerOpen }) {
+import { API_ENDPOINTS } from '../../utils/Mapping';
+import { ApiRouter } from '../../utils/Api';
+
+export default function Course({ setDrawerOpen }) {
     return (
-        <>
+        <Box sx={{ backgroundColor: 'rgba(5,10,14,1.00)', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', p: 3, px: 10 }}>
+            <IconButton sx={{ position: 'absolute', top: 64, left: 64, color: 'white' }} onClick={setDrawerOpen}>
+                <MenuIcon />
+            </IconButton>
             <iframe src="/legacy/home.html" title="Legacy Home Page" style={{ width: '100%',  height: '100vh', border: 'none' }}/>
-        </>
+        </Box>
     );
 }
