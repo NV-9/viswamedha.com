c99fec0fb577e08344f72e3591f85c72dd23447d
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-18 17:45:25 +0000

    Added more helper functions

--- src/utils/Helpers.jsx
+++ src/utils/Helpers.jsx
@@ -1,3 +1,6 @@
+import { API_ENDPOINTS } from './Mapping';
+import { ApiRouter } from './Api';
+import { format, parseISO, isSameDay, isBefore } from 'date-fns';
 
 
 export const formatDate = (dateString) => {
@@ -22,4 +25,43 @@ export const getSubDomain = () => {
     if (host.includes('localhost'))
         return (parts.length === 1) ? null : parts.slice(0, -1).join('.');
     return host.split('.').slice(0, -2).join('.');
+}
+
+export const routeToHomeIfNotLoggedIn = (navigate) => {
+    ApiRouter.get(API_ENDPOINTS.SESSION())
+    .then(data => {
+        if (!data.isAuthenticated) {
+            navigate('/');
+        }
+    });
+}
+
+export const routeToHomeIfNotAdmin = (navigate) => {
+    ApiRouter.get(API_ENDPOINTS.SESSION())
+    .then(data => {
+        if (!data.isStaff) {
+            navigate('/');
+        }
+    });
+}
+
+export function formatLessonDate(startString, endString) {
+    const startDate = parseISO(startString);
+    const endDate = parseISO(endString);
+
+    if (isSameDay(startDate, endDate)) {
+        return `${format(startDate, "MMMM d, yyyy")} at ${format(startDate, "h:mm a")} - ${format(endDate, "h:mm a")}`;
+    } else {
+        return `${format(startDate, "MMMM d, yyyy 'at' h:mm a")} - ${format(endDate, "MMMM d, yyyy 'at' h:mm a")}`;
+    }
+}
+
+export const convertIsoToDateTime = (isoString) => {
+    return parseISO(isoString);
+}
+
+export function isLessonCompleted(endDateString) {
+    const endDate = parseISO(endDateString);
+    const now = new Date(); 
+    return isBefore(endDate, now);
 }
