94c76f96e7fe4ed12258b2337c3f2bf35b64a4e9
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-20 00:51:43 +0000

    Refactored styling and validation for login and signup

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,73 +1,70 @@
-import { Alert, Box, Button, Collapse, Container, FormControl, IconButton, Input, InputLabel, Typography } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Alert, Box, Button, Card, CardContent, Collapse, Container, FormControl, Grid2 as Grid, IconButton, Input, InputLabel, Typography} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { ApiRouter } from '../../utils/Api';
-import { API_ENDPOINTS } from '../../utils/Mapping';
+import { API_ENDPOINTS, mapping } from '../../utils/Mapping';
 
-export default function Login() {
+export default function Login({ setDrawerOpen }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errorShown, setErrorShown] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
-        ApiRouter.get(API_ENDPOINTS.SESSION())
-        .then(data => {
-            if (data.isAuthenticated) {
-                navigate('/profile/');
-            }
+        ApiRouter.get(API_ENDPOINTS.SESSION()).then(data => {
+            if (data.isAuthenticated) navigate('/profile/');
         });
     }, []);
 
     const loginRequest = () => {
-        const userLoginData = {
-            username: username,
-            password: password,
-        }
-        ApiRouter.post(API_ENDPOINTS.LOGIN(), userLoginData)
-        .then(data => {
-            if (data.success == true) {
-                navigate('/profile/');
-            } else {
+        const userLoginData = { username, password };
+        ApiRouter.post(API_ENDPOINTS.LOGIN(), userLoginData).then(data => {
+            if (data.success) navigate('/profile/');
+            else {
                 setPassword('');
                 setErrorShown(true);
             }
-        })
-    }
+        });
+    };
 
     return (
-        <Container maxWidth="xs" sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-            <Box component="form" sx={{ width: '100%', padding: 4, boxShadow: 3, borderRadius: 2 }}>
-                <Typography variant="h5" textAlign="center" gutterBottom>
-                    Login
-                </Typography>
-                <Collapse in={errorShown}>
-                <Alert variant="outlined" severity="warning" sx={{ mb: 2 }}
-                    action={
-                        <IconButton aria-label="close" color="inherit" size="small" onClick={() => setErrorShown(false)}>
-                            <CloseIcon fontSize="inherit" />
-                        </IconButton>
-                    }>
-                    Incorrect username or password!
-                </Alert>
-            </Collapse>
-                <FormControl fullWidth sx={{ mb: 3 }}>
-                    <InputLabel htmlFor="username">Username</InputLabel>
-                    <Input id="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete='on' />
-                </FormControl>
-                <FormControl fullWidth sx={{ mb: 3 }}>
-                    <InputLabel htmlFor="password">Password</InputLabel>
-                    <Input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete='on'/>
-                </FormControl>
-                <Button variant="contained" color="primary" fullWidth sx={{ mb: 2 }} onClick={loginRequest} disabled={!username || !password}>
-                    Login
-                </Button>
-                <Button variant="outlined" color="secondary" fullWidth onClick={() => navigate('/signup/')}>
-                    Sign Up
-                </Button>
-            </Box>
-        </Container>
+        <Box sx={{ backgroundColor: 'rgba(5,10,14,1.00)', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', p: 0, px: { xs: 2, sm: 3 } }}>
+            <IconButton sx={{ position: 'absolute', top: 64, left: 64, color: 'white' }} onClick={setDrawerOpen}>
+                <MenuIcon />
+            </IconButton>
+            <Container maxWidth="xs" sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+                <Card sx={{ width: '100%', padding: 4, boxShadow: 3, borderRadius: 2 }}>
+                    <CardContent>
+                        <form>
+                            <Typography variant="h5" textAlign="center" gutterBottom>Login</Typography>
+                            <Collapse in={errorShown}>
+                                <Alert variant="outlined" severity="warning" sx={{ mb: 2 }} action={
+                                    <IconButton aria-label="close" color="inherit" size="small" onClick={() => setErrorShown(false)}>
+                                        <CloseIcon fontSize="inherit" />
+                                    </IconButton>
+                                }>
+                                    Incorrect username or password!
+                                </Alert>
+                            </Collapse>
+                            <Grid container spacing={2}>
+                                <FormControl fullWidth sx={{ mb: 3 }}>
+                                    <InputLabel htmlFor="username">Username</InputLabel>
+                                    <Input id="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="on" />
+                                </FormControl>
+                                <FormControl fullWidth sx={{ mb: 3 }}>
+                                    <InputLabel htmlFor="password">Password</InputLabel>
+                                    <Input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="on" />
+                                </FormControl>
+                            </Grid>
+                            <Button variant="contained" color="primary" fullWidth sx={{ mb: 2 }} onClick={loginRequest} disabled={!username || !password}>Login</Button>
+                            <Button variant="outlined" color="secondary" fullWidth onClick={() => navigate(mapping['Signup'].getPath())}>Sign Up</Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Container>
+        </Box>
     );
 }
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,15 +1,14 @@
-import { Alert, Button, Collapse, IconButton } from '@mui/material';
-import { Container, Box, Typography, FormControl, InputLabel, Input } from '@mui/material';
-import Grid from '@mui/material/Grid2';
+import { Alert, Button, Box, Card, CardContent, Collapse, Container, FormControl, Grid2 as Grid, IconButton, Input, InputLabel, Typography } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { ApiRouter } from '../../utils/Api';
-import { API_ENDPOINTS } from '../../utils/Mapping';
-import { validateEmail, validatePassword } from '../../utils/Validation';
+import { API_ENDPOINTS, mapping } from '../../utils/Mapping';
+import { validateUsername, validateEmail, validatePassword } from '../../utils/Validation';
 
-export default function Signup() {
+export default function Signup({ setDrawerOpen}) {
     const [username, setUsername] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [firstName, setFirstName] = useState('');
@@ -37,13 +36,19 @@ export default function Signup() {
     }, [username, emailAddress, firstName, lastName, dateOfBirth, password, confirmPassword])
 
     const signupRequest = () => {
+        if (!validateUsername(username)) {
+            setErrorMessage('Username must be at least 3 characters and contain only alphanumeric and lowercase characters.');
+            setErrorShown(true);
+            return;
+        }
+
         if (!validateEmail(emailAddress)) {
             setErrorMessage('Please enter a valid email address.');
             setErrorShown(true);
             return;
         }
         if (!validatePassword(password)) {
-            setErrorMessage('Password must be at least 8 characters, include an uppercase letter, a lowercase letter, a number, and a special character.');
+            setErrorMessage('Password must be at least 8 characters, include an uppercase letter, a lowercase letter, a number, and a special character. Special characters include @$!%*?&');
             setErrorShown(true);
             return;
         }
@@ -75,97 +80,86 @@ export default function Signup() {
     }
 
     return (
-        (<Container maxWidth="xs" sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-            <Box component="form" sx={{ width: '100%', padding: 4, boxShadow: 3, borderRadius: 2 }}>
-                <Typography variant="h5" textAlign="center" gutterBottom>
-                    Sign Up
-                </Typography>
-                <Collapse in={errorShown}>
-                    <Alert
-                        severity="error"
-                        action={
-                            <IconButton aria-label="close" color="inherit" size="small" onClick={() => setErrorShown(false)}>
-                                <CloseIcon fontSize="inherit" />
-                            </IconButton>
-                        }
-                        sx={{ mb: 2 }}
-                    >
-                        {errorMessage}
-                    </Alert>
-                </Collapse>
-                <Grid container spacing={2} sx={{ mb: 3 }}>
-                    <Grid size={{ xs: 12,  md: 6  }}>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="username">Username</InputLabel>
-                            <Input id="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="on" />
-                        </FormControl>
-                    </Grid>
-                    <Grid
-                        size={{
-                            xs: 12,
-                            md: 6
-                        }}>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="emailAddress">Email Address</InputLabel>
-                            <Input id="emailAddress" type="email" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} autoComplete="on" />
-                        </FormControl>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={2} sx={{ mb: 3 }}>
-                    <Grid
-                        size={{
-                            xs: 12,
-                            md: 6
-                        }}>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="firstName">First Name</InputLabel>
-                            <Input id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} autoComplete="on" />
-                        </FormControl>
-                    </Grid>
-                    <Grid
-                        size={{
-                            xs: 12,
-                            md: 6
-                        }}>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="lastName">Last Name</InputLabel>
-                            <Input id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} autoComplete="on" />
-                        </FormControl>
-                    </Grid>
-                </Grid>
-                <FormControl fullWidth sx={{ mb: 3 }}>
-                    <InputLabel htmlFor="dateOfBirth">Date Of Birth</InputLabel>
-                    <Input id="dateOfBirth" type="date" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} autoComplete="on" />
-                </FormControl>
-                <Grid container spacing={2} sx={{ mb: 3 }}>
-                    <Grid
-                        size={{
-                            xs: 12,
-                            md: 6
-                        }}>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="on" />
-                        </FormControl>
-                    </Grid>
-                    <Grid
-                        size={{
-                            xs: 12,
-                            md: 6
-                        }}>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
-                            <Input id="confirmPassword" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} autoComplete="on" />
-                        </FormControl>
-                    </Grid>
-                </Grid>
-                <Button variant="contained" color="primary" fullWidth sx={{ mb: 2 }} onClick={signupRequest} disabled={disabled}>
-                    Sign Up
-                </Button>
-                <Button variant="outlined" color="secondary" fullWidth onClick={() => navigate('/login/')}>
-                    Login
-                </Button>
-            </Box>
-        </Container>)
+        <Box sx={{ backgroundColor: 'rgba(5,10,14,1.00)', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', p: 0, px: { xs: 2, sm: 3 } }}>
+            <IconButton sx={{ position: 'absolute', top: 64, left: 64, color: 'white' }} onClick={setDrawerOpen}>
+                <MenuIcon />
+            </IconButton>
+            <Container maxWidth="xs" sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+                <Card sx={{ width: '100%', padding: 4, boxShadow: 3, borderRadius: 2 }}>
+                    <CardContent>
+                        <form>
+                            <Typography variant="h5" textAlign="center" gutterBottom>
+                                Sign Up
+                            </Typography>
+                            <Collapse in={errorShown}>
+                                <Alert
+                                    severity="error"
+                                    action={
+                                        <IconButton aria-label="close" color="inherit" size="small" onClick={() => setErrorShown(false)}>
+                                            <CloseIcon fontSize="inherit" />
+                                        </IconButton>
+                                    }
+                                    sx={{ mb: 2 }}
+                                >
+                                    {errorMessage}
+                                </Alert>
+                            </Collapse>
+                            <Grid container spacing={2} sx={{ mb: 3 }}>
+                                <Grid size={{ xs: 12,  md: 6  }}>
+                                    <FormControl fullWidth>
+                                        <InputLabel htmlFor="username">Username</InputLabel>
+                                        <Input id="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="on" />
+                                    </FormControl>
+                                </Grid>
+                                <Grid size={{ xs: 12, md: 6 }}>
+                                    <FormControl fullWidth>
+                                        <InputLabel htmlFor="emailAddress">Email Address</InputLabel>
+                                        <Input id="emailAddress" type="email" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} autoComplete="on" />
+                                    </FormControl>
+                                </Grid>
+                            </Grid>
+                            <Grid container spacing={2} sx={{ mb: 3 }}>
+                                <Grid size={{ xs: 12, md: 6 }}>
+                                    <FormControl fullWidth>
+                                        <InputLabel htmlFor="firstName">First Name</InputLabel>
+                                        <Input id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} autoComplete="on" />
+                                    </FormControl>
+                                </Grid>
+                                <Grid size={{ xs: 12, md: 6 }}>
+                                    <FormControl fullWidth>
+                                        <InputLabel htmlFor="lastName">Last Name</InputLabel>
+                                        <Input id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} autoComplete="on" />
+                                    </FormControl>
+                                </Grid>
+                            </Grid>
+                            <FormControl fullWidth sx={{ mb: 3 }}>
+                                <InputLabel htmlFor="dateOfBirth" shrink>Date Of Birth</InputLabel>
+                                <Input id="dateOfBirth" type="date" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} autoComplete="on" />
+                            </FormControl>
+                            <Grid container spacing={2} sx={{ mb: 3 }}>
+                                <Grid size={{ xs: 12, md: 6 }}>
+                                    <FormControl fullWidth>
+                                        <InputLabel htmlFor="password">Password</InputLabel>
+                                        <Input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="on" />
+                                    </FormControl>
+                                </Grid>
+                                <Grid size={{ xs: 12, md: 6 }}>
+                                    <FormControl fullWidth>
+                                        <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
+                                        <Input id="confirmPassword" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} autoComplete="on" />
+                                    </FormControl>
+                                </Grid>
+                            </Grid>
+                            <Button variant="contained" color="white" fullWidth sx={{ mb: 2 }} onClick={signupRequest} disabled={disabled}>
+                                Sign Up
+                            </Button>
+                            <Button variant="outlined" color="secondary" fullWidth onClick={() => navigate(mapping['Login'].getPath())}>
+                                Login
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Container>
+        </Box>
     );
 }
