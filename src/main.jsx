eb1e196fe28196a0f6a6f6fc18be8d2cbf89b52e
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-25 18:00:52 +0000

    Added localization provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-      	<BrowserRouter>
-			<App />
-		</BrowserRouter>
+      	<LocalizationProvider dateAdapter={AdapterDateFns}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+    	</LocalizationProvider>
     </StrictMode>,
 )
