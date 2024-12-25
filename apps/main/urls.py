2fe83a1d593b45e29f7fcb7990c479f4062d0fb2
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-25 21:55:38 +0000

    Added exclusive regex for media and static paths

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -4,5 +4,5 @@ from django.conf import settings
 from apps.main.views import serve_react
 
 urlpatterns = [
-    re_path(r"^(?P<path>.*)$", serve_react, {"document_root": settings.BUILD_DIR}),
+    re_path(r"^(?!static/|media/)(?P<path>.*)$", serve_react, {"document_root": settings.BUILD_DIR}),
 ]
