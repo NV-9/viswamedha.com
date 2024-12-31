70fe35cdedfe819db87f24c63e249595e3b0830f
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2024-12-31 12:10:12 +0000

    Added manual chunking

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,19 @@ export default defineConfig({
 	build: {
 		outDir: './build',
 		emptyOutDir: true,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						const librariesToKeepTogether = ['react', 'react-dom', 'react-transition-group', '@emotion', '@mui', 'mui', 'zustand', 'its-fine'];
+						const packageName = id.toString().split('node_modules/')[1].split('/')[0].toString();
+						if (librariesToKeepTogether.some(lib => packageName.includes(lib))) {
+							return 'react-vendor';
+						}
+						return packageName;
+					}
+				}
+			}
+		}
 	}
 })
