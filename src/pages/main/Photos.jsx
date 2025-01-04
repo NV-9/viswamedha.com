02f9d73736f7fbdd491e4fe80ed8941df7a1f5aa
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-01-04 21:20:12 +0000

    Added skeleton for photo loading

--- src/pages/main/Photos.jsx
+++ src/pages/main/Photos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Box,  Dialog, DialogContent, IconButton, ImageList, ImageListItem, Typography } from '@mui/material';
+import { Box,  Dialog, DialogContent, IconButton, ImageList, ImageListItem, Skeleton, Typography } from '@mui/material';
 import { MenuIcon } from '../../icon/MenuIcon';
   
 import { API_ENDPOINTS } from '../../utils/Endpoints';
@@ -8,6 +8,7 @@ import { ApiRouter } from '../../utils/Api';
 export default function Photos({ setDrawerOpen }) {
     const [photos, setPhotos] = useState([]);
     const [open, setOpen] = useState(false);
+    const [loadedImages, setLoadedImages] = useState({});
     const [selectedPhoto, setSelectedPhoto] = useState(null);
   
     useEffect(() => {
@@ -23,6 +24,10 @@ export default function Photos({ setDrawerOpen }) {
       setSelectedPhoto(null);
       setOpen(false);
     };
+
+    const handleImageLoad = (index) => {
+        setLoadedImages((prev) => ({ ...prev, [index]: true }));
+    };
   
     return (
         <Box sx={{ backgroundColor: 'rgba(5,10,14,1.00)', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', p: 3, px: { sm: 4, md: 10 } }}>
@@ -33,9 +38,22 @@ export default function Photos({ setDrawerOpen }) {
                 Photos
             </Typography>
             <ImageList cols={3} gap={8} variant={'masonry'}>
-                {photos && photos.map((photo, index) => (
+                {photos.map((photo, index) => (
                     <ImageListItem key={index} onClick={() => handleOpen(photo)}>
-                        <img srcSet={`${photo.image}?w=248&fit=crop&auto=format&dpr=2 2x`} src={`${photo.image}?w=164&fit=crop&auto=format`} alt={photo.alt} loading="lazy" style={{ cursor: 'pointer' }}/>
+                        {!loadedImages[index] && (
+                            <Skeleton variant="rectangular" width="100%" height={200} />
+                        )}
+                        <img
+                            srcSet={`${photo.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                            src={`${photo.image}?w=164&fit=crop&auto=format`}
+                            alt={photo.alt}
+                            loading="lazy"
+                            onLoad={() => handleImageLoad(index)}
+                            style={{
+                                display: loadedImages[index] ? 'block' : 'none',
+                                cursor: 'pointer',
+                            }}
+                        />
                     </ImageListItem>
                 ))}
             </ImageList>
