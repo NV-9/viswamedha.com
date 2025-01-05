6c3eff4369647eee18bf4f22f1cf0e5f7fce84e6
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-01-05 00:31:33 +0000

    Added daphne run command

--- config/asgi.py
+++ config/asgi.py
@@ -4,13 +4,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from apps.chat.routing import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+django_asgi_app = get_asgi_application()
+
+from apps.chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
