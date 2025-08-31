82a8b437bf2cda62d412ac286d0080e612b2f619
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-08-31 17:54:34 +0100

    Modified cv to be accessed referentially

--- src/pages/main/About.jsx
+++ src/pages/main/About.jsx
@@ -6,13 +6,6 @@ import { API_ENDPOINTS } from '../../utils/Endpoints';
 import profileImage from '/img/About.png';
 
 export default function About({ setDrawerOpen }) {
-    const [socialLinks, setSocialLinks] = useState(null);
-
-    useEffect(() => {
-        ApiRouter.get(API_ENDPOINTS.SOCIALS())
-        .then(setSocialLinks);
-    }, []);
-
     return (
         <Box sx={{ backgroundColor: 'rgba(5,10,14,1.00)', backgroundSize: 'cover', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', p: 2, px: { xs: 2, sm: 2, md: 10, lg: 10}, position: 'relative', color: 'white', height: {md: '100vh', lg: '100vh'} }}>
            <IconButton sx={{ position: 'absolute', top: { xs: 16, md: 64 }, left: { xs: 16, md: 64 }, color: 'white' }} onClick={setDrawerOpen}>
@@ -32,7 +25,7 @@ export default function About({ setDrawerOpen }) {
                     <Typography variant="body1" sx={{ color: 'white', mb: 3 }}>
                         I have experience in Full Stack, Ethical Hacking, Machine Learning, Software Engineering & Web Development.
                     </Typography>
-                    <Button variant="contained" color="primary" href={socialLinks ? socialLinks.cv : ''} sx={{ color: 'white'}}>
+                    <Button variant="contained" color="primary" href={`/${API_ENDPOINTS.REFERENCE("cv")}`} sx={{ color: 'white'}}>
                         Download CV
                     </Button>
                 </Box>
